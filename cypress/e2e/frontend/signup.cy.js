eab984e87e6b85a85316d4f51403c1bd63c11919
Allan Sklarow <allan@sklarow.blog.br>
2025-01-30 11:56:36 +0000

    test: add admin user creation test

--- cypress/e2e/frontend/signup.cy.js
+++ cypress/e2e/frontend/signup.cy.js
@@ -2,5 +2,9 @@ describe("Signup Test", () => {
     it("Create a new user successfully", () => {      
       cy.createTestUserViaUI();
     });
+
+    it("Create a new admin user successfully", () => {      
+      cy.createTestUserViaUI({admin: true});
+    });
   });
   
