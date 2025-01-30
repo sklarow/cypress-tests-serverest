dfb0340584f7ae311553b34b3624163d4063ade0
Allan Sklarow <allan@sklarow.blog.br>
2025-01-30 13:49:34 +0000

    refactor: small typo

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://front.serverest.dev/",
+    baseUrl: "https://front.serverest.dev",
     env: {
-      apiBaseUrl: "https://serverest.dev/",
+      apiBaseUrl: "https://serverest.dev",
       defaultPassword: "teste",
     },
     setupNodeEvents(on, config) {
