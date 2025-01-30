01d9d7a3a1ef18142732e95736a52e96511fc908
Allan Sklarow <allan@sklarow.blog.br>
2025-01-30 13:01:40 +0000

    refactor: add apiBaseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://front.serverest.dev/",
     env: {
+      apiBaseUrl: "https://serverest.dev/",
       defaultPassword: "teste",
     },
     setupNodeEvents(on, config) {
